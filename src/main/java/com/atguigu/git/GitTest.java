d27a924abd5147ca9bd11c1eea1e4fe71759d6da
mengqi <mengqi2099@163.com>
2022-05-11 21:31:59 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,10 +4,11 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world!1");
         System.out.println("hello world!2");
-        System.out.println("hello world!  3");
+        System.out.println("hello world!3");
         System.out.println("hello world!4 hot-fix");
         System.out.println("hello world!5 hot-fix");
         System.out.println("hello world!5 master");
         System.out.println("hello world!6");
+        System.out.println("push test");
     }
 }
